848d12cb8478bf03e3717a36f6b601f4aa4d3823
Reque Bernardo <requejpr.123@gmail.com>
2019-08-28 17:16:01 -0400

    actualziar

--- asignaciones/proyect_final/ciudades/contacto/js/time.js
+++
@@ -1,3 +0,0 @@
-var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
-var f=new Date();
-document.write(f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear() + "  " + f.getHours() + ":" + f.getMinutes() + ":" + f.getSeconds());
