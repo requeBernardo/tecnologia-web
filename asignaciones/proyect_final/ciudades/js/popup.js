259076b23dbc5a8970322bbe332e85d55b6ba150
Reque Bernardo <requejpr.123@gmail.com>
2019-08-28 16:34:07 -0400

    actualziar

---
+++ asignaciones/proyect_final/ciudades/js/popup.js
@@ -0,0 +1,36 @@
+var OpenPopUp = document.getElementById("open-popup"),
+    overlay = document.getElementById("overlay"),
+    popup = document.getElementById("popup"),
+    closePopup = document.getElementById("close-popup");
+var requestURL = 'https://requebernardo.github.io/tecnologia-web/asignaciones/asignacion_9/ciudades/JOSN/tiwanaku.json';
+var request = new XMLHttpRequest();
+var information = document.getElementById('information');
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+    var place = request.response;
+    populateinformation(place);
+}
+function populateinformation(jsonObj) {
+    var myH1 = document.createElement('h2');
+    myH1.textContent = jsonObj['city'];
+    information.appendChild(myH1);
+
+    var myH1 = document.createElement('h3');
+    myH1.textContent = jsonObj['place'];
+    information.appendChild(myH1);
+
+    var myPara = document.createElement('p');
+    myPara.textContent = 'Descripcion: ' + jsonObj['description'];
+    information.appendChild(myPara);
+}
+
+OpenPopUp.addEventListener('click', function(){
+    overlay.classList.add('active');
+});
+
+closePopup.addEventListener('click', function(){
+    overlay.classList.remove('active')
+});
---
+++ asignaciones/proyect_final/ciudades/js/weather-lapaz.js
@@ -0,0 +1,21 @@
+let weatherRequest = new XMLHttpRequest ();
+// URL http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}
+// APIKEY: ###################
+// Identificación de Preston: 5604473
+let apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=3911925&units=metric&APPID=61d21ebc5ce74da5540c27ee619264cd'
+weatherRequest.open ('GET', apiURLstring, true);
+weatherRequest.send ();
+/* Para esperar a que se cargue el objeto de solicitud, use el método onload para
+encapsular
+el analizador JSON y las declaraciones de salida en una función genérica O usan un
+addEventListener usando el parámetro load. */
+ weatherRequest.onload = function () {
+ let weatherData = JSON.parse (weatherRequest.responseText);
+ console.log (weatherData);
+ document.getElementById ('cc-temp'). innerHTML = weatherData.main.temp;
+
+ let icon ="http://openweathermap.org/img/wn/" + weatherData.weather[font.icon+".png"];
+ let desc = weatherData.weather [0] .description;
+ document.getElementById ('cc-img'). setAttribute ('src', icono);
+ document.getElementById ('cc-img'). setAttribute ('alt', desc);
+ }
