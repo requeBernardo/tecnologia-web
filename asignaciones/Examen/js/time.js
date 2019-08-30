dd6d7b15a4a43d1add3261f3e8f30266db40f6cd
Reque Bernardo <requejpr.123@gmail.com>
2019-08-29 22:02:17 -0400

    commit

---
+++ asignaciones/Examen/js/time.js
@@ -0,0 +1,3 @@
+var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+var f=new Date();
+document.write(f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear());
